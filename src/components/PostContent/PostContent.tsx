7fd3cf02387570b34414de10ccc027032a84fe6a
leetosc <leetosc@hotmail.com>
2024-11-12 01:36:33 -0600

    feat: use img from frontmatter for og

--- src/components/PostContent/PostContent.tsx
+++ src/components/PostContent/PostContent.tsx
@@ -11,7 +11,7 @@ const PostContent = (props: IPostContentProps) => (
   <div className="mx-auto mt-5 max-w-prose">
     <div className="aspect-h-2 aspect-w-3">
       <img
-        className="h-full w-full rounded-lg object-cover object-center"
+        className="size-full rounded-lg object-cover object-center"
         src={props.content.imgSrc}
         alt={props.content.imgAlt}
         loading="lazy"
